0e0449897cc82c93037861bcc9e05f36cbec3400
beaandresdesign <beatriz.andres@alumnos.udit.es>
2026-01-10 12:21:00 +0100

    segundo commit --areglar posición titulo inicio

---
+++ 04_carrousel.js
@@ -0,0 +1,21 @@
+const carouselInner = document.querySelector('.carousel-inner');
+const items = document.querySelectorAll('.carousel-item');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+    if (i < 0) index = items.length - 1;
+    else if (i >= items.length) index = 0;
+    else index = i;
+
+    carouselInner.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// Botones
+prevBtn.addEventListener('click', () => showSlide(index - 1));
+nextBtn.addEventListener('click', () => showSlide(index + 1));
+
+// Cambiar automáticamente cada 3 segundos
+setInterval(() => showSlide(index + 1), 3000);
