b49cb5a15584321629027a2f3388c4bbd244d316
beaandresdesign <beatriz.andres@alumnos.udit.es>
2026-01-11 18:49:45 +0100

    commit06____retoques

--- 04_carrousel.js
+++ 04_carrousel.js
@@ -7,7 +7,7 @@ let index = 0;
 
 function showSlide(i) {
     if (i < 0) index = items.length - 1;
-    else if (i >= items.length) index = 0;
+    if (i >= items.length) index = 0;
     else index = i;
 
     carouselInner.style.transform = `translateX(-${index * 100}%)`;
